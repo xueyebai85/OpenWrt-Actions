3054c34317d5b56aeef39b51b6364b0735c62186
ChangeYuan <59167936+yunxi993@users.noreply.github.com>
2021-10-03 15:56:36 +0800

    Update diy-Selfuse.sh

--- diy-Selfuse.sh
+++ diy-Selfuse.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
 
 # Hostname
 sed -i 's/OpenWrt/qnmlgb/g' package/base-files/files/bin/config_generate
