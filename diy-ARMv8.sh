dc93b0b56a840751bef026abd0cfdd0a1533d259
ChangeYuan <59167936+yunxi993@users.noreply.github.com>
2021-10-21 02:52:49 +0800

    Update diy-ARMv8.sh

--- diy-ARMv8.sh
+++ diy-ARMv8.sh
@@ -30,8 +30,8 @@ sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/theme
 # Add additional packages
 git clone https://github.com/fw876/helloworld.git package/helloworld
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
