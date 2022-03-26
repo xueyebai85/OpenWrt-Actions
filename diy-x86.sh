7810b20523a5a19c5275a09710d5c01f307235e5
ChangeYuan <59167936+yunxi993@users.noreply.github.com>
2022-03-27 00:54:48 +0800

    Update diy-x86.sh

--- diy-x86.sh
+++ diy-x86.sh
@@ -24,14 +24,13 @@ sed -i 's/OpenWrt/qnmlgb/g' package/base-files/files/bin/config_generate
 # sed -i 's/services/system/g' package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
 
 # Change default theme
-sed -i 's#luci-theme-bootstrap#luci-theme-neobird#g' feeds/luci/collections/luci/Makefile
+sed -i 's#luci-theme-bootstrap#luci-theme-opentomcat#g' feeds/luci/collections/luci/Makefile
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # Add additional packages
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/openwrt-passwall/luci-app-passwall
-git clone --depth=1 https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
 git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 
 # dockerd去版本验证
