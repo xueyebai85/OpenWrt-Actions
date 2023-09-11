ca00f8b1125c747eafef6848dd09e8da4f21ffc0
ChangeYuan <59167936+yunxi993@users.noreply.github.com>
2023-09-11 12:30:25 +0800

    update: modify package repo url.

--- original/diy-x86.sh
+++ original/diy-x86.sh
@@ -29,7 +29,7 @@ sed -i 's/OpenWrt/nzksm/g' package/base-files/files/bin/config_generate
 
 # Add additional packages
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone -b packages --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
 #git clone --depth=1 -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
