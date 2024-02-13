f3d7b296e373400db262a89d9b798d6d9a26255b
xueyebai <zhongrenwei@gmail.com>
2024-02-14 00:14:21 +0800

    Update diy-part1.sh

--- lede/diy-part1.sh
+++ lede/diy-part1.sh
@@ -14,8 +14,16 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> 'feeds.conf.default'
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> 'feeds.conf.default'
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> 'feeds.conf.default'
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> 'feeds.conf.default'
+# echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 
-sed -i "48a\\
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config\n\
-" package/lean/default-settings/files/zzz-default-settings
+# add a passwall
+
+# add a theme-argon
+cd package/lean
+rm -rf *argon*
+git clone -b 18.06 https://github.com/xueyebai/luci-theme-argon.git
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git
