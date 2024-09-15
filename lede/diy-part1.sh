4cad1543882eeccee7ecbbca82ff29a6c69fa4a1
xueyebai <zhongrenwei@gmail.com>
2024-09-15 22:10:06 +0800

    Update diy-part1.sh

--- lede/diy-part1.sh
+++ lede/diy-part1.sh
@@ -20,8 +20,11 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> 'feeds.conf.default'
 echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest.git' >>feeds.conf.default
 
-# add a theme-argon
+# add plugins
 cd package/lean
-rm -rf *argon*
+rm -rf *argon* *mosdns*
 git clone -b 18.06 https://github.com/xueyebai/luci-theme-argon.git
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git
+wget https://github.com/sbwml/luci-app-mosdns/archive/refs/tags/v5.3.1.tar.gz
+tar -zxvf *tar.gz
+git clone -b master https://github.com/fw876/helloworld
