d61f508e3c5a87ad61e9c925d0a1d1c2f7dafb1f
xueyebai <zhongrenwei@gmail.com>
2024-11-21 14:35:14 +0800

    Update diy-part1.sh

--- lede/diy-part1.sh
+++ lede/diy-part1.sh
@@ -21,4 +21,5 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >
 # echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest.git' >>feeds.conf.default
 
 # add plugins
+cd package/lean
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git
