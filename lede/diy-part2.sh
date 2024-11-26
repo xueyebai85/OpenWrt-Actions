6659e9736f7bc68907110b322d76fb9a0805dc5b
xueyebai <zhongrenwei@gmail.com>
2024-11-26 15:32:06 +0800

    Update diy-part2.sh

--- lede/diy-part2.sh
+++ lede/diy-part2.sh
@@ -21,6 +21,6 @@ git clone -b 23.x https://github.com/sbwml/packages_lang_golang feeds/packages/l
 sed -i 's/+luci-theme-bootstrap /  \\\n        +luci-theme-argon +luci-app-argon-config  \\\n        +luci-app-openclash +luci-app-passwall  \\\n        +luci-app-ddns +luci-app-smartdns +wget-nossl +luci-app-filetransfer +luci-app-upnp  /g' feeds/luci/collections/luci/Makefile
 #sed -i 's/+luci-app-samba4 //g' package/lean/autosamba/Makefile
 
-cd feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img
-sudo rm -rf bg1.jpg
-sudo wget https://github.com/xueyebai/OpenWrt-Actions/edit/main/lede/bg1.jpg
+# cd feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img
+# sudo rm -rf bg1.jpg
+# sudo wget https://github.com/xueyebai/OpenWrt-Actions/edit/main/lede/bg1.jpg
