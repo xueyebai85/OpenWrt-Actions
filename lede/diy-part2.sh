db327becbad3db0511676bcab8667c83f6a279c0
xueyebai <zhongrenwei@gmail.com>
2024-12-07 02:27:48 +0800

    Create diy-part2.sh

---
+++ lede/diy-part2.sh
@@ -0,0 +1,15 @@
+#
+#
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+##修改插件
+cd feeds/luci/applications
+rm -rf *passwall*
+#rm -rf *clash*
+
+git clone https://github.com/xiaorouji/openwrt-passwall.git
+git clone https://github.com/xiaorouji/openwrt-passwall2.git
+mv openwrt-passwall/luci-app-passwall .
+mv openwrt-passwall2/luci-app-passwall2 .
+rm -rf *openwrt-pa*
