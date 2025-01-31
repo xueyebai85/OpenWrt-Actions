2cdd4d7b9857e86b53bd9c6ee54569cce6ed4765
xueyebai85 <xueyebai85@gmail.com>
2025-01-31 09:43:09 +0800

    Update diy-part1.sh

--- lede/diy-part1.sh
+++ lede/diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 # echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> 'feeds.conf.default'
-# echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> 'feeds.conf.default'
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> 'feeds.conf.default'
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> 'feeds.conf.default'
 # echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> 'feeds.conf.default'
 # echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest.git' >>feeds.conf.default
